00beddd3ce3949792695c05b5099d8445fc3ef20
samtasticc <samantha.henesy@gmail.com>
2024-09-19 15:37:45 -0400

    set up project structure

---
+++ server.js
@@ -0,0 +1,15 @@
+// ========= IMPORTS ========= //
+require('dotenv').config() // this imports and converts, it has to be the first line of code.
+const express = require("express");
+const mongoose = require('mongoose')
+const app = express();
+// ========= MONGOOSE ========= //
+
+
+
+// ========= MIDDLEWARE ========= //
+
+
+
+// ========= ROUTES ========= //
+
