8d91abf9d6227360ff37518eb2930c34e29012eb
samtasticc <samantha.henesy@gmail.com>
2024-09-19 17:15:10 -0400

    build out the view and form in new.ejs for creating new items

--- models/planets.js
+++ models/planets.js
@@ -4,7 +4,8 @@ const planetSchema = new mongoose.Schema({
     name: String,
     hasRing: Boolean,
     hasMoon: Boolean,
-    innerOrOuterPlanet: Boolean,
+    innerPlanet: Boolean,
+    outerPlanet: Boolean,
 })
 
 const Planet = mongoose.model('Planet', planetSchema) // using an uppercase letter at the beginning is how we create the model
